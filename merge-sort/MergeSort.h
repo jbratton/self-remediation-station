1a11312810cc985418268a61f8a9f8b3a0c54f75
Jeremy Bratton <jeremy@alumni.rice.edu>
2013-01-19 13:48:03 -0600

    initial commit of merge-sort

---
+++ merge-sort/MergeSort.h
@@ -0,0 +1,5 @@
+#import <Foundation/NSObject.h>
+
+@interface MergeSort : NSObject
++ (NSMutableArray*)sortArray:(NSArray*)input;
+@end
---
+++ merge-sort/MergeSort.m
@@ -0,0 +1,8 @@
+#import "MergeSort.h"
+
+@implementation MergeSort
++ (NSMutableArray*)sort:(NSArray*)input {
+	NSLog(@"Hello, World!");
+	return [[NSMutableArray alloc] init];
+}
+@end
