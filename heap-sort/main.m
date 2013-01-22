aaa1efc7a1708a21e547f50f261f4c514e453680
Jeremy Bratton <jeremy@alumni.rice.edu>
2013-01-21 21:41:18 -0600

    Added basic Heap test.

--- heap-sort/main.m
+++ heap-sort/main.m
@@ -1,6 +1,22 @@
+#import <Foundation/Foundation.h>
 #import "Heap.h"
 
 int main(void) {
+	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
 
+	NSArray *inputArrays = @[
+		@[@6, @4, @2, @0, @6, @7, @8],
+		@[@1, @2, @3, @4, @5, @6, @7],
+		@[@7, @6, @5, @4, @3, @2, @1],
+		@[@32, @3, @36, @75, @74, @-1, @36, @-1, @6, @9, @60, @63, @65, @87, @3]
+	];
+
+	for (id array in inputArrays) {
+		NSLog(@"array is like: %@", [array componentsJoinedByString:@", "]);
+		Heap *h = [[Heap alloc] initWithArray:array];
+		NSLog(@"heap is like:\n%@", h);
+	}
+
+	[pool drain];
 	return 0;
 }
